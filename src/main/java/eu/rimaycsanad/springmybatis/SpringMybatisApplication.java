800f8c67c7088ae584239ad05b21e694c1b684dd
Csanád Rimay <rimaycsanad@gmail.com>
2020-03-18 09:33:45 +0100

    jndi datasource example

--- src/main/java/eu/rimaycsanad/springmybatis/SpringMybatisApplication.java
+++ src/main/java/eu/rimaycsanad/springmybatis/SpringMybatisApplication.java
@@ -1,19 +1,25 @@
 package eu.rimaycsanad.springmybatis;
 
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
 @SpringBootApplication
 @MapperScan("eu.rimaycsanad.springmybatis.mapper")
 public class SpringMybatisApplication {
+
+	@Value("${datasource.jndi.name:jdbc/ClavisDS}")
+	private String dsJndi;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMybatisApplication.class, args);
@@ -26,6 +32,16 @@ public class SpringMybatisApplication {
 				.addScript("schema.sql")
 				.addScript("testData.sql")
 				.build();
+	}
+
+	@Bean
+	public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+		JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+		bean.setJndiName(dsJndi);
+		bean.setProxyInterface(DataSource.class);
+		bean.setLookupOnStartup(false);
+		bean.afterPropertiesSet();
+		return (DataSource) bean.getObject();
 	}
 
 	@Bean
